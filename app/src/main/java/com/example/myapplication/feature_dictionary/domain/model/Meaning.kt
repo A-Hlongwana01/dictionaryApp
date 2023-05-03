5b039e6c356bb045e4062846fc374e17874e0c3c
A-Hlongwana01 <ayabonggahlongwana@gmail.com>
2023-05-03 15:17:09 +0200

    removed unnecessary imports

--- app/src/main/java/com/example/myapplication/feature_dictionary/domain/model/Meaning.kt
+++ app/src/main/java/com/example/myapplication/feature_dictionary/domain/model/Meaning.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.feature_dictionary.domain.model
 
-import com.example.myapplication.feature_dictionary.data.remote.dto.Definition
 
 data class Meaning(
     val definitions: List<Definition>,
