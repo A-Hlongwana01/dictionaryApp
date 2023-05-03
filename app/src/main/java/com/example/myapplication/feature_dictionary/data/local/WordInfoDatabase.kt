bd3d241af61666c9a78eb094e776c7a45c0934d5
A-Hlongwana01 <ayabonggahlongwana@gmail.com>
2023-05-03 20:30:48 +0200

    update

--- app/src/main/java/com/example/myapplication/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/com/example/myapplication/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication.feature_dictionary.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.plcoding.dictionary.feature_dictionary.data.local.Converters
+import com.plcoding.dictionary.feature_dictionary.data.local.WordInfoDao
 import com.plcoding.dictionary.feature_dictionary.data.local.entity.WordInfoEntity
 
 @Database(
     entities = [WordInfoEntity::class],
     version = 1
 )
-class WordInfoDatabase {
+@TypeConverters(Converters::class)
+abstract class WordInfoDatabase: RoomDatabase() {
+
+    abstract val dao: WordInfoDao
 }
