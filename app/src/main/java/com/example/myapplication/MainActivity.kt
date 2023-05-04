b7b2d2b8ec97612a8eda29914279faccbe90fbd7
A-Hlongwana01 <ayabonggahlongwana@gmail.com>
2023-05-04 09:50:59 +0200

    ignore

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,21 +3,89 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import org.w3c.dom.Text
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myapplication.feature_dictionary.presentaion.WordInfoViewModel
+import com.example.myapplication.feature_dictionary.presentation.WordInfoItem
+import com.example.myapplication.ui.theme.DictionaryTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world")
+            DictionaryTheme {
+                val viewModel: WordInfoViewModel = hiltViewModel()
+                val state = viewModel.state.value
+                val scaffoldState = rememberScaffoldState()
+
+                LaunchedEffect(key1 = true) {
+                    viewModel.eventFlow.collectLatest { event ->
+                        when(event) {
+                            is WordInfoViewModel.UIEvent.ShowSnackbar -> {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = event.message
+                                )
+                            }
+                        }
+                    }
+                }
+                Scaffold(
+                    scaffoldState = scaffoldState
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.color)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                        ) {
+                            TextField(
+                                value = viewModel.searchQuery.value,
+                                onValueChange = viewModel::onSearch,
+                                modifier = Modifier.fillMaxWidth(),
+                                placeholder = {
+                                    Text(text = "Search...")
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                items(state.wordInfoItems.size) { i ->
+                                    val wordInfo = state.wordInfoItems[i]
+                                    if(i > 0) {
+                                        Spacer(modifier = Modifier.height(8.dp))
+                                    }
+                                    WordInfoItem(wordInfo = wordInfo)
+                                    if(i < state.wordInfoItems.size - 1) {
+                                        Divider()
+                                    }
+                                }
+                            }
+                        }
+                        if(state.isLoading) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                        }
+                    }
+                }
+            }
         }
     }
 }
